5251e5ab76978be8bc6764c23dc4387f616348d7
Oga-1129 <kdg.taiga.ogata@gmail.com>
2020-02-04 23:31:24 +0900

    パッケージの再インストール

---
+++ Assets/AWS01/src/Image.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Image : MonoBehaviour
+{
+    string url = "";
+
+    // Start is called before the first frame update
+    [System.Obsolete]
+    IEnumerator Sample()
+    {
+        WWW www = new WWW(url);
+
+        // 画像ダウンロード完了を待機
+        yield return www;
+
+        // webサーバから取得した画像をRawImagで表示する
+        RawImage rawImage = GetComponent<RawImage>();
+        rawImage.texture = www.textureNonReadable;
+        
+    }
+
+    public void seturl(){
+        // wwwクラスのコンストラクタに画像URLを指定
+        url = "https://co-test-aws.s3-ap-northeast-1.amazonaws.com/co-test-aws/test/nyanko.jpg";
+        StartCoroutine ("Sample");
+    }
+}
---
+++ Assets/AWS01/src/dropdown.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+public class dropdown : MonoBehaviour 
+{
+    public int selectNum;
+    public TMP_Dropdown dpdown;
+	public void OnValueChanged(int result)
+	{
+		// 処理
+        selectNum = dpdown.value;
+	}
+}
