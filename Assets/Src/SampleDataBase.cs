78da9b7fc8239c754a87b7dc6cf841213e9c43ed
Oga-1129 <kdg.taiga.ogata@gmail.com>
2020-02-04 12:14:32 +0900

    Setting SQLite
    SQLiteの設定

---
+++ Assets/Src/SampleDataBase.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SampleDataBase : MonoBehaviour {
+
+    public SqliteDatabase sqlDB;
+
+    void Start(){
+　　　　　sqlDB = new SqliteDatabase("PlayerStatus.db");
+　　　　　string query = "SELECT * FROM example";
+　　　　　var dt = sqlDB.ExecuteQuery(query);
+
+        string name;
+        int num;
+        foreach(DataRow dr in dt.Rows){
+            name = (string)dr["name"];
+            num = (int)dr["num"];
+            Debug.Log("name:" + name.ToString());
+            Debug.Log("num:" + num);
+        }
+    }
+}
